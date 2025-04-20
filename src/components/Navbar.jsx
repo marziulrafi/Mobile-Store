e87309783cfcc2d066f75dbd4520f368f2a314a9
K M Marziul Karim <marziulrafi@gmail.com>
2025-04-20 12:27:50 +0600

    Banner

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { MdShoppingCart, MdBookmarkAdd } from "react-icons/md";
 
 const Navbar = () => {
     return (
-        <div className="navbar p-0 bg-base-100 shadow-sm">
+        <div className="navbar p-0 bg-base-100 shadow-sm mx-auto px-8 md:px-12 lg:px-16 xl:px-24">
   <div className="navbar-start">
     <div className="dropdown">
       <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Hero from '../components/Hero';
+import MobileContainer from '../components/MobileContainer';
 
 const Home = () => {
     return (
         <div>
-            
+            <Hero/>
+            <MobileContainer/>
         </div>
     );
 };
